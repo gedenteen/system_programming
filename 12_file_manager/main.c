ebf1bf551f4cce95b6996cac14c97bac4e1d1437
gedenteen <razumow.dmitrij@yandex.ru>
2021-03-11 18:01:01 +0300

    Implement navigate when count of files > rows of terminal

--- 12_file_manager/main.c
+++ 12_file_manager/main.c
@@ -5,11 +5,34 @@
 #include <curses.h> //ncurses
 #include <dirent.h> //для просмотра каталогов и файлов
 #include <string.h>
+#include <malloc.h>
+
+int min(int a, int b) {
+	if (a < b)
+		return a;
+	else
+		return b;
+}
 
 void handling_SIGWINCH(int signo) { //обработчик сигнала SIGWINCH
 	struct winsize size; //структура для размеров окна
 	ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size); //получить размеры окна
 	resizeterm(size.ws_row, size.ws_col); //изменить размеры окна
+}
+
+int print_files(WINDOW *wnd, struct dirent **namelist, int n, 
+	        int upper_bound, int lower_bound) {
+	werase(wnd); //заполнить окно пробелами
+	for (int i = upper_bound; i < min(n, lower_bound); i++) { 
+		//if (i == chosen_row)
+		//	wattron(wnd, COLOR_PAIR(1));
+		//else
+		//	wattron (wnd, COLOR_PAIR(2));
+		wmove(wnd, i - upper_bound, 0);
+		wprintw(wnd, "%s", namelist[i]->d_name);
+		wrefresh(wnd);
+	}	        
+	return 0;	        
 }
 
 int main() {
@@ -19,17 +42,22 @@ int main() {
 	initscr();
 	signal(SIGWINCH, handling_SIGWINCH);
 	cbreak();
-	curs_set(0); //курсор невидимый
-	start_color();
-	refresh();
-	wnd1 = newwin(20, 30, 0, 0); //сначала размеры окна, потом координата верхнего левого угла в stdscr
-	box(wnd1, '|', '-');
-	subwnd1 = derwin(wnd1, 18, 28, 1, 1); //под-окно, чтобы не затереть границы окна
+	curs_set(FALSE); //курсор невидимый
+	start_color(); //начать работу с цветом терминала (ncurses)
+	//refresh();
+	
+	struct winsize size; //структура для размеров окна
+	ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size); //получить размеры окна
+	int mid_of_terminal = size.ws_col / 2;
+	wnd1 = newwin(size.ws_row, mid_of_terminal, 0, 0); //задать размеры окна
+	box(wnd1, '|', '-'); //символы для границ окна
+	//под-окно, чтобы не затереть границы окна:
+	subwnd1 = derwin(wnd1, size.ws_row - 2, mid_of_terminal - 2, 1, 1); 
 	//wprintw(subwnd1, "Hello, brave new curses world!\n");
 	wrefresh(wnd1);
 	
 	/// второе окно: ///
-	wnd2 = newwin(20, 30, 0, 30);
+	wnd2 = newwin(size.ws_row, mid_of_terminal, 0, mid_of_terminal); 
 	init_pair(1, COLOR_BLACK, COLOR_YELLOW); 
 	init_pair(2, COLOR_WHITE, COLOR_BLACK);
 	wbkgd(wnd2, COLOR_PAIR(1) | A_BOLD);
@@ -40,14 +68,15 @@ int main() {
 	wrefresh(wnd2);
 	
 	struct dirent **namelist;
-	int n, chosen_row = 0;
-	char* dir_name = (char*) malloc(sizeof(char) * 2);
-	strcpy(dir_name, ".");
-	n = scandir(dir_name, &namelist, 0, alphasort);
+	int n, chosen_row = 0; //кол-во файлов, выбранная строка (файл)
+	int upper_bound = 0, lower_bound = size.ws_row - 2; //границы выводимых файлов
+	char* dir_name = (char*) malloc(sizeof(char) * 2); //путь к выбранной директории
+	strcpy(dir_name, "."); //изначально выбранная директория = папка, откуда запускается программа
+	n = scandir(dir_name, &namelist, 0, alphasort); //получить массив имен файлов (отсортированный)
 	if (n < 0)
 		perror("scandir");
 	else {
-		for (int i = 0; i < n; i++) { 
+		for (int i = upper_bound; i < min(n, lower_bound); i++) { 
 			if (i == chosen_row)
 				wattron(subwnd1, COLOR_PAIR(1));
 			else
@@ -59,20 +88,39 @@ int main() {
 	}
 	
 	char ch;
-	wmove(subwnd1, 0, 1); //переместиться за точку, чтобы ничего не ломать
+	wmove(subwnd1, 0, mid_of_terminal - 5); //переместиться за точку, чтобы не перекрывать
 	//надо отключить вывод символов
 	while ((ch = wgetch(subwnd1)) != 10) {
 		if (ch == 119 && chosen_row > 0) { //'w'
-			wattron(subwnd1, COLOR_PAIR(2));
-			wmove(subwnd1, chosen_row, 0);
-			wprintw(subwnd1, "%s", namelist[chosen_row]->d_name);
-			chosen_row--;
+			if (chosen_row == upper_bound && upper_bound > 0) {
+				upper_bound--;
+				lower_bound--;
+				chosen_row--;
+				print_files(subwnd1, namelist, n, upper_bound, lower_bound);\
+			}
+			else {	
+				wattron(subwnd1, COLOR_PAIR(2));
+				wmove(subwnd1, chosen_row - upper_bound, 0);
+				wprintw(subwnd1, "%s", namelist[chosen_row]->d_name);
+				chosen_row--;
+			}
 		}
-		else if (ch == 115 && chosen_row < n - 1) { //'s'
-			wattron(subwnd1, COLOR_PAIR(2));
-			wmove(subwnd1, chosen_row, 0);
-			wprintw(subwnd1, "%s", namelist[chosen_row]->d_name);
-			chosen_row++;
+		else if (ch == 115) {
+			//если выделена самая нижняя строка, но ниже есть еще файлы:
+			if (chosen_row == lower_bound - 1 && chosen_row < n - 1) {
+				upper_bound++;
+				lower_bound++;
+				chosen_row++;
+				print_files(subwnd1, namelist, n, upper_bound, lower_bound);
+			}
+			else if (chosen_row < n - 1) { //'s'
+				wattron(subwnd1, COLOR_PAIR(2));
+				wmove(subwnd1, chosen_row - upper_bound, 0);
+				wprintw(subwnd1, "%s", namelist[chosen_row]->d_name);
+				chosen_row++;
+			}
+			else
+				chosen_row = n - 1;
 		}
 		else if (ch == 113) { //'q'
 			delwin(subwnd1); //удалить структуру, но текст останется
@@ -83,48 +131,58 @@ int main() {
 			exit(EXIT_SUCCESS);
 		}
 		else if (ch == 101) { //'e'
-			//char *new_dir;
-			//добавить имя папки к сохраненному пути:
-			strcat(dir_name, "/");
-			strcat(dir_name, namelist[chosen_row]->d_name); 
-			if (opendir(dir_name) == NULL) { //
+			//временная строка для проверка нового пути:
+			char *new_dir_name;
+			//ее размер = старый путь + '\' + новая папка + '\0'
+			int new_dir_len = strlen(dir_name) + strlen(namelist[chosen_row]->d_name) + 2;
+			new_dir_name = (char*) malloc(sizeof(char) * new_dir_len);
+			strcpy(new_dir_name, dir_name); //скопировать текущий путь
+			strcat(new_dir_name, "/"); //добавить в конец пути новую папку
+			strcat(new_dir_name, namelist[chosen_row]->d_name);
+			if (opendir(new_dir_name) == NULL) { //если указан файл, а не папка
+				free(new_dir_name);
 				wmove(subwnd1, 0, 10); 
 				wprintw(subwnd1, "this isn't a file");
 				wrefresh(subwnd1);
 				continue;
 			}
+			free(new_dir_name); //временная строка больше не нужна
+			dir_name = realloc(dir_name, sizeof(char) * new_dir_len);
+			strcat(dir_name, "/"); 
+			strcat(dir_name, namelist[chosen_row]->d_name); 
 			
-			chosen_row = 0;
+			chosen_row = 0; //выбранная строка в новой директории - это '.'
 			werase(subwnd1); //заполнить окно пробелами
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < n; i++) //очистить память для массива файлов
 				free(namelist[i]);
-			}
 			free(namelist);
-			n = scandir(dir_name, &namelist, 0, alphasort);
+			n = scandir(dir_name, &namelist, 0, alphasort); //получить новый массив
 			
-			wmove(subwnd1, 0, 5); wprintw(subwnd1, "n=%d", n); //
-			for (int i = 0; i < n; i++) { 
+			//wmove(subwnd1, 0, 5); wprintw(subwnd1, "n=%d", n); //удалить
+			for (int i = upper_bound; i < min(n, lower_bound); i++) { 
 				if (i == chosen_row)
 					wattron(subwnd1, COLOR_PAIR(1));
 				else
 					wattron (subwnd1, COLOR_PAIR(2));
-				wmove(subwnd1, i, 0);
+				wmove(subwnd1, i  - upper_bound, 0);
 				wprintw(subwnd1, "%s", namelist[i]->d_name);
-				wmove(subwnd1, 0, 10); wprintw(subwnd1, "i=%d", i); //
+				//wmove(subwnd1, 0, 10); wprintw(subwnd1, "i=%d", i); //удалить
 				wrefresh(subwnd1);
 			}
-			wmove(subwnd1, 0, 15); wprintw(subwnd1, "ok"); //
+			//wmove(subwnd1, 0, 15); wprintw(subwnd1, "ok"); //удалить
 		}
 		wattron(subwnd1, COLOR_PAIR(1));
-		wmove(subwnd1, chosen_row, 0);
+		wmove(subwnd1, chosen_row - upper_bound, 0);
 		wprintw(subwnd1, "%s", namelist[chosen_row]->d_name);
 		wattron (subwnd1, COLOR_PAIR(2));
 		wrefresh(subwnd1);
 		
-		wmove(subwnd1, 0, 1);
+		wmove(subwnd1, 0, mid_of_terminal - 5);
 	}
 	
-	//TODO: нужно освобождение памяти namelist
+	//TODO: баг при открытии CUDA
+	//TODO: нужно освобождение памяти namelist и dir_name
+	//TODO: обработка ошибок
 	delwin(subwnd1); //удалить структуру, но текст останется
 	delwin(wnd1);
 	delwin(wnd2);
