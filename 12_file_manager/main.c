0a5fc6a0253d6fab1a20fe76ec8fa8599baa475e
gedenteen <razumow.dmitrij@yandex.ru>
2021-03-14 16:09:24 +0300

    Finish the code, now user can use both windows

--- 12_file_manager/main.c
+++ 12_file_manager/main.c
@@ -88,6 +88,52 @@ int Key_down(WINDOW *wnd, struct dirent **namelist, int n,
 	return 0;
 }
 
+int Key_choose_file(WINDOW *wnd, struct dirent ***namelist, int *n,
+	char **dir_name, int mid_of_terminal,
+	int *chosen_row, int *upper_bound, int *lower_bound) 
+{
+	//временная строка для проверка нового пути:
+	char *new_dir_name;
+	//ее размер = старый путь + '\' + новая папка + '\0'
+	int new_dir_len = strlen(*dir_name) + strlen((*namelist)[*chosen_row]->d_name) + 2;
+	if (new_dir_len <= 0) {
+		endwin(); //конец работы с ncurses
+		printf("error: invalid value new_dir_len in function Key_choose_file()\n");
+		exit(1);
+	}
+	new_dir_name = (char*) malloc(sizeof(char) * new_dir_len);
+	strcpy(new_dir_name, *dir_name); //скопировать текущий путь
+	strcat(new_dir_name, "/"); //добавить в конец пути новую папку
+	strcat(new_dir_name, (*namelist)[*chosen_row]->d_name);
+	if (opendir(new_dir_name) == NULL) { //если указан файл, а не папка
+		free(new_dir_name);
+		wmove(wnd, 0, 10); 
+		wprintw(wnd, "this isn't a folder");
+		wrefresh(wnd);
+		wmove(wnd, 0, mid_of_terminal - 5);
+		return 1;
+	}
+	free(new_dir_name); //временная строка больше не нужна
+	//проделать все то же самое с основной строкой:
+	*dir_name = realloc(*dir_name, sizeof(char) * new_dir_len);
+	strcat(*dir_name, "/"); 
+	strcat(*dir_name, (*namelist)[*chosen_row]->d_name); 
+	//изменить данные:
+	*chosen_row = 0; //выбранная строка в новой директории - это '.'
+	werase(wnd); //заполнить окно пробелами
+	for (int i = 0; i < *n; i++) //очистить память для массива файлов
+		free((*namelist)[i]);
+	free(*namelist);
+	*n = scandir(*dir_name, *&namelist, 0, alphasort); //получить новый массив
+	if (*n < 0) {
+		endwin(); //конец работы с ncurses
+		printf("error: can't scandir() for current window in function Key_choose_file()\n");
+		exit(1);
+	}
+	Print_files(wnd, *namelist, *n, *upper_bound, *lower_bound, *chosen_row);
+	return 0;
+}
+
 int main() {
 	initscr();
 	signal(SIGWINCH, Handling_SIGWINCH);
@@ -134,16 +180,19 @@ int main() {
 	init_pair(2, COLOR_WHITE, COLOR_BLACK); //цвет для остальных файлов
 
 	//--------- ФАЙЛЫ ДЛЯ ЛЕВОГО ОКНА ---------//
-	struct dirent **namelist;
-	int n, chosen_row = 0; //всего файлов, выбранная строка (файл)
-	int upper_bound = 0, lower_bound = size.ws_row - 2; //границы выводимых файлов
-	char* dir_name = (char*) malloc(sizeof(char) * 2); //путь к выбранной директории
-	strcpy(dir_name, "."); //изначально выбранная директория = папка, откуда запускается программа
-	n = scandir(dir_name, &namelist, 0, alphasort); //получить массив имен файлов (отсортированный)
-	if (n < 0)
-		perror("scandir");
+	struct dirent **namelist1;
+	int n1, chosen_row1 = 0; //всего файлов, выбранная строка (файл)
+	int upper_bound1 = 0, lower_bound1 = size.ws_row - 2; //границы выводимых файлов
+	char* dir_name1 = (char*) malloc(sizeof(char) * 2); //путь к выбранной директории
+	strcpy(dir_name1, "."); //изначально выбранная директория = папка, откуда запускается программа
+	n1 = scandir(dir_name1, &namelist1, 0, alphasort); //получить массив имен файлов (отсортированный)
+	if (n1 < 0) {
+		endwin(); //конец работы с ncurses
+		printf("error: can't scandir() for 1st window\n");
+		exit(1);
+	}
 	else 
-		Print_files(subwnd1, namelist, n, upper_bound, lower_bound, chosen_row);
+		Print_files(subwnd1, namelist1, n1, upper_bound1, lower_bound1, chosen_row1);
 		
 	//--------- ФАЙЛЫ ДЛЯ ПРАВОГО ОКНА ---------//
 	struct dirent **namelist2;
@@ -152,15 +201,19 @@ int main() {
 	char* dir_name2 = (char*) malloc(sizeof(char) * 2); //путь к выбранной директории
 	strcpy(dir_name2, "."); //изначально выбранная директория = папка, откуда запускается программа
 	n2 = scandir(dir_name2, &namelist2, 0, alphasort); //получить массив имен файлов (отсортированный)
-	if (n < 0)
-		perror("scandir");
+	if (n2 < 0) {
+		endwin(); //конец работы с ncurses
+		printf("error: can't scandir() for 2nd window\n");
+		exit(1);
+	}
 	else 
 		Print_files(subwnd2, namelist2, n2, upper_bound2, lower_bound2, chosen_row2);
 	
-	int current_window = 1;
-	char ch;
+	//--------- ОБРАБОТКА КЛАВИШ ---------//
+	int current_window = 1; //текущее окно левое (1) или правое (2)
+	char ch; //переменная для wgetch() 
 	wmove(subwnd1, 0, mid_of_terminal - 5); //переместиться за точку, чтобы не перекрывать
-	while ((ch = wgetch(subwnd1)) != 10) {
+	while ((ch = wgetch(subwnd1)) != 10) { 
 		if (ch == 'a') { //клавиша влево - переключиться на 1-ое окно
 			current_window = 1;
 		}
@@ -168,44 +221,22 @@ int main() {
 			current_window = 2;
 		}
 		else if (ch == 'w') { //клавиша вверх - переключиться на файл выше
-			/*if (chosen_row == upper_bound && upper_bound > 0) {
-				upper_bound--;
-				lower_bound--;
-				chosen_row--;
-				Print_files(subwnd1, namelist, n, upper_bound, lower_bound, chosen_row);
-			}
-			else {	
-				Change_chosen_row(subwnd1, namelist, upper_bound, &chosen_row, -1);
-			}*/
 			if (current_window == 1)
-				Key_up(subwnd1, namelist, n, &chosen_row, &upper_bound, &lower_bound);
+				Key_up(subwnd1, namelist1, n1, &chosen_row1, &upper_bound1, &lower_bound1);
 			else
 				Key_up(subwnd2, namelist2, n2, &chosen_row2, &upper_bound2, &lower_bound2);
 		}
 		else if (ch == 115) { //'s'
-			/*
-			//если выделена самая нижняя строка, но ниже есть еще файлы:
-			if (chosen_row == lower_bound - 1 && chosen_row < n - 1) {
-				upper_bound++;
-				lower_bound++;
-				chosen_row++;
-				Print_files(subwnd1, namelist, n, upper_bound, lower_bound, chosen_row);
-			}
-			else if (chosen_row < n - 1) { //'s'
-				Change_chosen_row(subwnd1, namelist, upper_bound, &chosen_row, 1);
-			}
-			else
-				chosen_row = n - 1;*/
 			if (current_window == 1) 
-				Key_down(subwnd1, namelist, n, &chosen_row, &upper_bound, &lower_bound);
+				Key_down(subwnd1, namelist1, n1, &chosen_row1, &upper_bound1, &lower_bound1);
 			else
 				Key_down(subwnd2, namelist2, n2, &chosen_row2, &upper_bound2, &lower_bound2);
 		}
 		else if (ch == 113) { //'q'
-			for (int i = 0; i < n; i++) //очистить память для массива файлов
-				free(namelist[i]);
-			free(namelist);
-			free(dir_name); //очистить строку с путем по папкам
+			for (int i = 0; i < n1; i++) //очистить память для массива файлов
+				free(namelist1[i]);
+			free(namelist1);
+			free(dir_name1); //очистить строку с путем по папкам
 			delwin(subwnd1); //удалить структуры окон
 			delwin(wnd1);
 			delwin(wnd2);
@@ -213,35 +244,14 @@ int main() {
 			exit(EXIT_SUCCESS);
 		}
 		else if (ch == 101) { //'e'
-			//временная строка для проверка нового пути:
-			char *new_dir_name;
-			//ее размер = старый путь + '\' + новая папка + '\0'
-			int new_dir_len = strlen(dir_name) + strlen(namelist[chosen_row]->d_name) + 2;
-			new_dir_name = (char*) malloc(sizeof(char) * new_dir_len);
-			strcpy(new_dir_name, dir_name); //скопировать текущий путь
-			strcat(new_dir_name, "/"); //добавить в конец пути новую папку
-			strcat(new_dir_name, namelist[chosen_row]->d_name);
-			if (opendir(new_dir_name) == NULL) { //если указан файл, а не папка
-				free(new_dir_name);
-				wmove(subwnd1, 0, 10); 
-				wprintw(subwnd1, "this isn't a folder");
-				wrefresh(subwnd1);
-				wmove(subwnd1, 0, mid_of_terminal - 5);
-				continue;
-			}
-			free(new_dir_name); //временная строка больше не нужна
-			//проделать все то же самое с основной строкой:
-			dir_name = realloc(dir_name, sizeof(char) * new_dir_len);
-			strcat(dir_name, "/"); 
-			strcat(dir_name, namelist[chosen_row]->d_name); 
-			//изменить данные:
-			chosen_row = 0; //выбранная строка в новой директории - это '.'
-			werase(subwnd1); //заполнить окно пробелами
-			for (int i = 0; i < n; i++) //очистить память для массива файлов
-				free(namelist[i]);
-			free(namelist);
-			n = scandir(dir_name, &namelist, 0, alphasort); //получить новый массив
-			Print_files(subwnd1, namelist, n, upper_bound, lower_bound, chosen_row);
+			if (current_window == 1)
+				Key_choose_file(subwnd1, &namelist1, &n1, 
+					&dir_name1, mid_of_terminal,
+					&chosen_row1, &upper_bound1, &lower_bound1);
+			else
+				Key_choose_file(subwnd2, &namelist2, &n2, 
+					&dir_name2, mid_of_terminal,
+					&chosen_row2, &upper_bound2, &lower_bound2);
 		}
 		wmove(subwnd1, 0, mid_of_terminal - 5);
 	}
@@ -249,6 +259,8 @@ int main() {
 	//TODO: баг при открытии CUDA
 	//TODO: обработка ошибок
 	//TODO: мб надо отключить вывод символов
+	//TODO: ИЗМЕНИТЬ while ((ch = wgetch(subwnd1)) != 10)
+	//TODO: массив из окон и других переменных?
 	/*
 	for (int i = 0; i < n; i++) //очистить память для массива файлов
 		free(namelist[i]);
