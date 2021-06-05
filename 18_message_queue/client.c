e19337485fe6822a143c5841b4021ad00727bda6
gedenteen <gedenteen@gmail.com>
2021-06-05 08:28:00 +0700

    Add chat version 4

--- 18_message_queue/client.c
+++ 18_message_queue/client.c
@@ -1,7 +1,93 @@
 #include "header.h"
 
+#include <termios.h>
+#include <sys/ioctl.h> //для обработки сигналов
+#include <signal.h> //для соединения сигнала и функции-обработчика
+#include <curses.h> //ncurses
+
+//TODO посмотреть какие библиотеки для чего нужны
+
+#define RIGHT_WND_WIDTH 23
+#define BOTTOM_WND_HEIGHT 5
+
+void Handling_SIGWINCH(int signo) 
+{ //обработчик сигнала SIGWINCH
+	struct winsize size; //структура для размеров окна
+	ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size); //получить размеры окна
+	resizeterm(size.ws_row, size.ws_col); //изменить размеры окна
+}
+
+int CreateWindow(WINDOW **wnd, WINDOW **subwnd, int height, int width, int x, int y, char* text) 
+{
+	*wnd = newwin(height, width, x, y); //создать окно(высота, ширина, координаты левого верхнего угла)
+	if (*wnd == NULL)
+		return 1;
+	//box(*wnd, '|', '-'); //символы для границ окна
+	wborder(*wnd, '|', '|', '=', '=', '|', '|', '|', '|');
+	/// добавление надписи:
+	wmove(*wnd, 0, 4);
+	wattron(*wnd, COLOR_PAIR(2));
+	wprintw(*wnd, text);
+	wattron(*wnd, COLOR_PAIR(1));
+	
+	*subwnd = derwin(*wnd, height - 2, width - 2, 1, 1); //под-окно, чтобы не стирать границы окна
+	if (*subwnd == NULL)
+		return 1;
+	wrefresh(*wnd);
+	return 0;
+}
+
 int main (int argc, char **argv)
 {
+	initscr();
+	signal(SIGWINCH, Handling_SIGWINCH);
+	cbreak(); //...
+	curs_set(TRUE); //курсор видимый 
+	noecho(); //отключить вывод вводимых символов
+	
+	struct winsize size; //структура для размеров окна
+	ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size); //получить размеры окна
+	int mid_of_terminal = size.ws_col / 2;
+	
+	//--------- ЦВЕТ ---------//
+	start_color(); //начать работу с цветом терминала (ncurses)
+	init_pair(1, COLOR_WHITE, COLOR_BLACK); //цвет для остальных файлов
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK); //цвет для выделения выбранного файла
+	
+	//--------- СОЗДАНИЕ ОКОН  ---------//
+	WINDOW *wndChat, *wndUsers, *wndInput;
+	WINDOW *subwndChat, *subwndUsers, *subwndInput;
+	int currentHeight, currentWidth;
+	//первое окно:
+	int status = CreateWindow(&wndChat, &subwndChat, 
+		size.ws_row - BOTTOM_WND_HEIGHT, size.ws_col - RIGHT_WND_WIDTH, //размер окна
+		0, 0,
+		" Chat "); //координаты левого верхнего угла
+	if (status) {
+		printf("error\n");
+		//exit(EXIT_FAILURE);
+	}
+	//второе окно:
+	status = CreateWindow(&wndUsers, &subwndUsers, 
+		size.ws_row - BOTTOM_WND_HEIGHT, RIGHT_WND_WIDTH, //размер окна
+		0, size.ws_col - RIGHT_WND_WIDTH,
+		" Users "); //координаты левого верхнего угла
+	if (status) { 
+		printf("error\n");
+		//exit(EXIT_FAILURE);
+	}
+	//3-ье окно:
+	status = CreateWindow(&wndInput, &subwndInput, 
+		BOTTOM_WND_HEIGHT, size.ws_col, //размер окна
+		size.ws_row - BOTTOM_WND_HEIGHT, 0,
+		" Your message (press Enter to send) "); //координаты левого верхнего угла
+	if (status) {
+		printf("error\n");
+		//exit(EXIT_FAILURE);
+	}	
+
+	//==========================================================//
+
 	/// mq = message queue (очередь сообщений)
     char clientName[64];
     sprintf (clientName, "/sp-example-client-%d", getpid());
@@ -57,16 +143,18 @@ int main (int argc, char **argv)
             continue;
         }
 
-        // receive response from server
+        /// получить сообщение от сервера: 
 		char inBuffer[MAX_MSG_SIZE];
         if (mq_receive (mqClient, inBuffer, MSG_BUFFER_SIZE, NULL) == -1) {
             perror ("Client: mq_receive");
             exit (1);
         }
+       
+        ///
+       
         // display token received from server
-        printf ("Client: Token received from server: %s\n\n", inBuffer);
-
-        printf ("Ask for a token (Press ): ");
+        //printf ("Client: Token received from server: %s\n\n", inBuffer);
+        //printf ("Ask for a token (Press ): ");
     }
 
 
@@ -81,5 +169,10 @@ int main (int argc, char **argv)
     }
     printf ("Client: bye\n");
 
-    exit (0);
+
+	//удалить структуры окон
+	delwin(subwndChat), delwin(subwndUsers), delwin(subwndUsers);  
+	delwin(wndChat), delwin(wndUsers), delwin(wndInput); 
+	endwin(); //конец работы с ncurses
+    exit(EXIT_SUCCESS);
 }
