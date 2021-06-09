344a57a63a8c302b7fcdfcad1be058e93d7ee1dd
gedenteen <gedenteen@gmail.com>
2021-06-09 17:40:33 +0700

    Implement inet sockets

---
+++ 19_sockets/inet/header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h> 
+#include <sys/types.h> //для сокетов
+#include <sys/socket.h> //для сокетов
+#include <netinet/in.h> //для sockaddr_in
+#include <stdlib.h> //exit()
+#include <unistd.h> //для работы с файлами: close(), unlink()
+
+#define BUFFER_SIZE 256
+
+#endif
