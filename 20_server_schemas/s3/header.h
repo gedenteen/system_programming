f8bd4a85d162bea19947fc164b1a54e07965b7d9
gedenteen <gedenteen@gmail.com>
2021-06-12 09:51:32 +0700

    Implement scheme 3

---
+++ 20_server_schemas/s3/header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h> 
+#include <sys/types.h> //для сокетов
+#include <sys/socket.h> //для сокетов
+#include <netinet/in.h> //для sockaddr_in
+#include <stdlib.h> //exit()
+#include <unistd.h> //для работы с файлами: close(), unlink()
+
+#define BUFFER_SIZE 256
+#define CNT_THREADS 10
+
+#define MQ_NAME "/sp-message-queue"
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 10
+#define MAX_MSG_SIZE 256
+#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
+
+#endif
